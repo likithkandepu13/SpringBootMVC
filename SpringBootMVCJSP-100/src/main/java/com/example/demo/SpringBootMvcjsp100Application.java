3c0a99d9cd472f7811aec51ed0a16367392fb465
Likith Kandepu <withlikith@gmail.com>
2026-05-29 17:45:51 +0530

    added basics

---
+++ SpringBootMVCJSP-100/src/main/java/com/example/demo/SpringBootMvcjsp100Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcjsp100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcjsp100Application.class, args);
+	}
+
+}
---
+++ SpringBootMVCJSP-100/src/main/webapp/Web-INF/views/insta.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome to Instagram ra!</title>
+</head>
+<body>
+hello andi bagunara
+</body>
+</html>
